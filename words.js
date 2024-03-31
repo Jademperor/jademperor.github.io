bf01cccd86c9fa33e793ad7ef1749665a9117ce9
Jademperor <161705635+Jademperor@users.noreply.github.com>
2024-03-31 16:35:23 +0800

    Add files via upload

---
+++ words.js
@@ -0,0 +1,74 @@
+const words = [
+  {
+    word:"addition",
+    hint:"The process of adding numbers"
+  },
+  {
+    word:"cuc",
+    hint:"The best university in the world"
+  },
+  {
+    word: "meeting",
+    hint: "Event in which people come together"
+  },
+  {
+    word: "number",
+    hint:"Math symbol used for counting"
+  },
+  {
+    word: "exchange",
+    hint: "The act of trading"
+  },
+  {
+    word: "canvas",
+    hint: "Piece of fabric for oil painting"
+  },
+  {
+    word: "garden",
+    hint: "Space for planting flower and plant"
+  },
+  {
+    word: "position",
+    hint: "Location of someone or something"
+  },
+  {
+    word: "feather",
+    hint: "Hair like outer covering of bird"
+  },
+  {
+    word: "comfort",
+    hint: "A pleasant feeling of relaxation"
+  },
+  {
+    word: "country",
+    hint: "A politically identified region"
+  },
+  {
+    word: "group",
+    hint: "A number of objects or persons"
+  },
+  {
+    word: "needle",
+    hint: "A thin and sharp metal pin"
+  },
+  {
+    word: "expert",
+    hint: "Person with extensive knowledge"
+  },
+  {
+    word: "statement",
+    hint: "A declaration of something"
+  },
+  {
+    word: "second",
+    hint: "One-sixtieth of a minute"
+  },
+  {
+    word: "second",
+    hint: "One-sixtieth of a minute"
+  },
+  {
+    word: "library",
+    hint: "place containing collection of books"
+  },
+]
